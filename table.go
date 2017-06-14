959692e086b84ce9515e441a9878f7f342514cb8
shesuyo <shesuyo@gmail.com>
2017-06-14 10:16:21 +0800

    Add Table IDIn

--- table.go
+++ table.go
@@ -5,49 +5,57 @@ import "fmt"
 import "strings"
 import "errors"
 
+// Table 是对CRUD进一层的封装
 type Table struct {
 	*CRUD
 	tableName string
 }
 
-// 返回这张表所有数据
+// All 返回这张表所有数据
 func (t *Table) All() []map[string]string {
 	return t.Query("SELECT * FROM " + t.tableName).RawsMap()
 }
 
-// 返回表有多少条数据
+// Count 返回表有多少条数据
 func (t *Table) Count() (count int) {
 	t.Query("SELECT COUNT(*) FROM " + t.tableName).Scan(&count)
 	return
 }
 
-// 查找表的更新时间
+// UpdateTime 查找表的更新时间
 func (t *Table) UpdateTime() (updateTime string) {
 	t.Query("SELECT `UPDATE_TIME` FROM  INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA =(select database()) AND TABLE_NAME = '" + t.tableName + "';").Scan(&updateTime)
 	return
 }
 
-// 查找表的自增ID的值
+// AutoIncrement 查找表的自增ID的值
 func (t *Table) AutoIncrement() (id int) {
 	t.Query("SELECT `AUTO_INCREMENT` FROM  INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA =(select database()) AND TABLE_NAME = '" + t.tableName + "';").Scan(&id)
 	return
 }
 
-// 设置自动增长ID
+// SetAutoIncrement 设置自动增长ID
 func (t *Table) SetAutoIncrement(id int) error {
 	return t.Exec("ALTER TABLE `" + t.tableName + "` AUTO_INCREMENT = " + strconv.Itoa(id)).err
 }
 
-// 查找表的最大ID，如果为NULL的话则为0
+// MaxID 查找表的最大ID，如果为NULL的话则为0
 func (t *Table) MaxID() (maxid int) {
 	t.Query("SELECT IFNULL(MAX(id), 0) as id FROM `" + t.tableName + "`").Scan(&maxid)
 	return
 }
 
-/*
-	创建
-	check 如果有，则会判断表里面以这几个字段为唯一的话，数据库是否存在此条数据，如果有就不插入了。
-*/
+// IDIn 查找多个ID对应的列
+func (t *Table) IDIn(ids ...interface{}) *SQLRows {
+	if len(ids) == 0 {
+		return &SQLRows{}
+	}
+	return t.Query(fmt.Sprintf("SELECT * FROM %s WHERE id in (%s)", t.tableName, argslice(len(ids))), ids...)
+}
+
+// Create 创建
+// check 如果有，则会判断表里面以这几个字段为唯一的话，数据库是否存在此条数据，如果有就不插入了。
+//
 func (t *Table) Create(m map[string]interface{}, checks ...string) error {
 	//INSERT INTO `feedback` (`task_id`, `template_question_id`, `question_options_id`, `suggestion`, `member_id`) VALUES ('1', '1', '1', '1', '1')
 	if len(checks) > 0 {
